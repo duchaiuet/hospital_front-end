e49e0284ae58d9b8aff88253120541b144833ed1
NguyenDucHai <duchaiuet@gmail.com>
2024-05-13 18:29:33 +0700

    fix: login google

--- src/sections/login/loginGoogle.jsx
+++ src/sections/login/loginGoogle.jsx
@@ -1,15 +1,23 @@
+import { jwtDecode } from "jwt-decode";
 import React, { useState } from 'react';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
-const clientId = '447499911959-09kgl3k46d3094ku9e0aikfo09ueiclu.apps.googleusercontent.com'; // Replace with your actual client ID
+
+
+const clientId = '447499911959-09kgl3k46d3094ku9e0aikfo09ueiclu.apps.googleusercontent.com';
+const scope = 'https://www.googleapis.com/auth/userinfo.profile';
+
 
 const LoginComponent = () => {
   const [key, setKey] = useState(0);
   const [isEmailExists, setIsEmailExists] = useState(null);
-  const onSuccess = (response) => {
-    console.log('response: ', response);
-    console.log('Login Successful:');
+  const onSuccess =  async (response) => {
+    const decoded = jwtDecode(response.credential);
+    console.log("decoded: ", decoded)
+    
+
     setKey((prevKey) => prevKey + 1);
+ 
   };
 
   const onError = (error) => {
@@ -25,7 +33,9 @@ const LoginComponent = () => {
           clientId={clientId}
           buttonText="Login with Google"
           onSuccess={onSuccess}
+          scope={scope}
           onError={onError}
+          useOneTap
         />
       )}
       {isEmailExists === false && <p>Email does not exist, please try again.</p>}
